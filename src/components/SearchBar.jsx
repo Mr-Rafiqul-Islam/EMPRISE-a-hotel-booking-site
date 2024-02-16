35083dfccf3eebf2054ea8791a20e2fd6bd54a02
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-17 01:14:36 +0600

    banner complete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import Subheading from './Subheading'
 import { CiLocationOn } from "react-icons/ci";
 import { MdOutlineArrowDropDownCircle } from "react-icons/md";
 import Date from '../assets/icons/Date';
+import Findicon from '../assets/icons/Findicon';
 
 function SearchBar({className}) {
   return (
@@ -29,16 +30,15 @@ function SearchBar({className}) {
               <MdOutlineArrowDropDownCircle className='text-3xl text-black/70 me-10'/>
             </form>
         </div>
-        <div className="ps-14">
+        <div className="ps-14 me-10">
             <span className='before:w-[70px] before:bg-[#f5d223] before:h-2 before:content-normal before:absolute before:-bottom-6 before:-z-1 relative'><Subheading text='Date' className='text-black text-[28px] font-poppins font-semibold '/></span>
-            <form className="flex items-center gap-10">
-              <select name="Location"  className="py-2 bg-white font-poppins font-normal text-[18px] text-[#99A3AD]  " id="Location">
-                <option selected >Set date</option>
-                <option value='sky diving' >Sky diving</option>
-                <option value='horse riding' >Horse riding</option>
-              </select>
-              <Date className='text-3xl text-black/70 me-10'/>
+            <form className="flex items-center">
+              <input type="datetime" placeholder='See date' className='underline py-2 bg-white font-poppins font-normal text-[18px] text-[#99A3AD] w-64 outline-none' name="" id="" />
+              <Date className='text-3xl text-black/70'/>
             </form>
+        </div>
+        <div className='w-full flex justify-end'>
+        <Findicon />
         </div>
     </div>
   )
