c5c2c90f88554d7c8419e4c071e6fbb14e57e256
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-20 14:55:01 +0600

    carousel

--- src/components/CardTour.jsx
+++ src/components/CardTour.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import Image from "./Image";
-import t1 from "../assets/images/t1.png";
-import t2 from "../assets/images/t2.png";
 import Subheading from "./Subheading";
 import Heading from "./Heading";
 import Paragraph from "./Paragraph";
 
-function CardTour() {
+function CardTour({img,serial,title}) {
   return (
     <div>
       <div className="flex">
         {/* image part */}
         <div className="relative">
-          <Image src={t1} />
+          <Image src={img} />
           <div className="w-[72px] h-[72px] grid place-content-center rounded-tr-2xl font-poppins text-2xl absolute bottom-0 left-0 bg-white ">
-            01
+            {serial}
           </div>
         </div>
         {/* text part */}
@@ -22,7 +20,7 @@ function CardTour() {
           <div className="pb-[63px] pt-[38px] ps-[38px] ">
             <Subheading text="Adventure Guru" className="text-third  mb-8" />
             <div className="w-[275px] mb-[110px]">
-              <Heading text="Martina James Junior" className=" font-medium " />
+              <Heading text={title} className=" font-medium " />
             </div>
             <div className="flex">
               <div>
