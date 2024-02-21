76d8f48174cd44608beb1d29a74bf68d8a7a31ee
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-02-21 23:44:11 +0600

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,60 +1,64 @@
-import React from 'react'
-import Banner from '../components/layouts/Banner'
-import Header from '../components/layouts/Header'
-import Category from '../components/layouts/Category'
-import Trending from '../components/layouts/Trending'
-import Destinations from '../components/layouts/Destinations'
-import BestSeller from '../components/layouts/BestSeller'
-import Tourguides from '../components/layouts/Tourguides'
-import Promotion from '../components/layouts/Promotion'
-import Newsletter from '../components/layouts/Newsletter'
-import Contact from '../components/layouts/Contact'
-import Footer from '../components/layouts/Footer'
+import React from "react";
+import Banner from "../components/layouts/Banner";
+import Header from "../components/layouts/Header";
+import Category from "../components/layouts/Category";
+import Trending from "../components/layouts/Trending";
+import Destinations from "../components/layouts/Destinations";
+import BestSeller from "../components/layouts/BestSeller";
+import Tourguides from "../components/layouts/Tourguides";
+import Promotion from "../components/layouts/Promotion";
+import Newsletter from "../components/layouts/Newsletter";
+import Contact from "../components/layouts/Contact";
+import Footer from "../components/layouts/Footer";
 
 // For Swiper core and required modules
-import { Navigation, } from 'swiper/modules';
+import { Navigation } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function Home() {
-  
-
   return (
     <section>
-        <Header/>
-        <Swiper
-      // install Swiper modules
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={1}
-      loop={true}
-      speed={1000}
-      navigation={{
-        nextEl: '.my-button-next',
-        prevEl: '.my-button-prev',
-      }}
-    >
-      <SwiperSlide><Banner/></SwiperSlide>
-      <SwiperSlide><Banner className='bg-banner2'/></SwiperSlide>
-      <SwiperSlide><Banner className='bg-banner3'/></SwiperSlide>
-    </Swiper>
-        <Category/>
-        <Trending/>
-        <Destinations/>
-        <BestSeller/>
-        <Tourguides/>
-        <Promotion/>
-        <Newsletter/>
-        <Contact/>
-        <Footer/>
+      <Header />
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation]}
+        spaceBetween={50}
+        slidesPerView={1}
+        loop={true}
+        speed={1000}
+        navigation={{
+          nextEl: ".my-button-next",
+          prevEl: ".my-button-prev",
+        }}
+      >
+        <SwiperSlide>
+          <Banner />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner className="bg-banner2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner className="bg-banner3" />
+        </SwiperSlide>
+      </Swiper>
+      <Category />
+      <Trending />
+      <Destinations />
+      <BestSeller />
+      <Tourguides />
+      <Promotion />
+      <Newsletter />
+      <Contact />
+      <Footer />
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
