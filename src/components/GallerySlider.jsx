8102fa7be778cf4bab204577887998a0214d3b6a
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-03-08 00:30:31 +0600

    breadcrumb

--- src/components/GallerySlider.jsx
+++ src/components/GallerySlider.jsx
@@ -1,28 +1,14 @@
 import React from "react";
 import Flex from "./Flex";
 import Image from "./Image";
-import a1 from "../assets/images/thumb1.png";
-import a2 from "../assets/images/thumb2.png";
-import a3 from "../assets/images/thumb3.png";
-import a4 from "../assets/images/thumb4.png";
 import beach from "../assets/images/Sandy.png";
 
-
 function GallerySlider() {
-  
   return (
     <div className="w-full">
-      <Flex className="gap-10">
-        <div className="flex flex-col gap-4">
-          <Image src={a1} />
-          <Image src={a2} />
-          <Image src={a3} />
-          <Image src={a4} />
-        </div>
-        <div>
-          <Image src={beach}/>
-        </div>
-      </Flex>
+      <div>
+        <Image src={beach} />
+      </div>
     </div>
   );
 }
--- src/pages/TourDetail.jsx
+++ src/pages/TourDetail.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import Header2 from '../components/layouts/Header2'
-import Container from '../components/Container'
-import GallerySlider from '../components/GallerySlider'
-import BestSeller from '../components/layouts/BestSeller'
+import React from "react";
+import Header2 from "../components/layouts/Header2";
+import Container from "../components/Container";
+import GallerySlider from "../components/GallerySlider";
+import BestSeller from "../components/layouts/BestSeller";
+import Breadcrumb from "../components/Breadcrumb";
+import Flex from "../components/Flex";
+import a1 from "../assets/images/thumb1.png";
+import a2 from "../assets/images/thumb2.png";
+import a3 from "../assets/images/thumb3.png";
+import a4 from "../assets/images/thumb4.png";
+import Image from "../components/Image";
 
 function TourDetail() {
   return (
     <>
-    <Header2/>
-    <Container>
-        <GallerySlider/>
-        <BestSeller title='Similar Tours'/>
-    </Container>
+      <Header2 />
+      <Container>
+        <Flex className="gap-10">
+          <div className="flex flex-col gap-4">
+            <Image src={a1} />
+            <Image src={a2} />
+            <Image src={a3} />
+            <Image src={a4} />
+          </div>
+          <Flex className="flex-col">
+            <GallerySlider />
+            <Breadcrumb className='mt-10' />
+          </Flex>
+        </Flex>
+        <BestSeller title="Similar Tours" />
+      </Container>
     </>
-  )
+  );
 }
 
-export default TourDetail
+export default TourDetail;
