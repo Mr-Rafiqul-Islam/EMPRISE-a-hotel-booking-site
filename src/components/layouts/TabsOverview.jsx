fbbce331ff48991b944274c6d00387431417fce0
mr-rafiqul-islam <rafiqulislamemon05@gmail.com>
2024-03-12 15:49:17 +0600

    tabs processing

--- src/components/layouts/TabsOverview.jsx
+++ src/components/layouts/TabsOverview.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Description from "../../assets/icons/descript.svg";
+import iternary from "../../assets/icons/inclusions.svg";
+import inclusions from "../../assets/icons/iternary.svg";
+import review from "../../assets/icons/review.svg";
+import faq from "../../assets/icons/faq.svg";
 import {
   Tabs,
   TabsHeader,
@@ -12,8 +16,8 @@ import { useState } from "react";
 import Paragraph from "../Paragraph";
 import { Link } from "react-router-dom";
 import Image from "../Image";
-import Subheading from '../Subheading'
-import watch from "../../assets/icons/watch.svg"
+import Subheading from "../Subheading";
+import watch from "../../assets/icons/watch.svg";
 
 function TabsOverview() {
   const [activeTab, setActiveTab] = useState("overview");
@@ -59,36 +63,36 @@ function TabsOverview() {
   ];
   const info = [
     {
-      title : 'Duration',
-      logo: '/src/assets/icons/watch.svg',
-      besideLogo: '2 Days',
+      title: "Duration",
+      logo: "/src/assets/icons/watch.svg",
+      besideLogo: "2 Days",
     },
     {
-      title : 'Activity',
-      logo: '/src/assets/icons/walking.svg',
-      besideLogo: 'Walking',
+      title: "Activity",
+      logo: "/src/assets/icons/walking.svg",
+      besideLogo: "Walking",
     },
     {
-      title : 'Physical',
-      logo: '/src/assets/icons/meter.svg',
-      besideLogo: 'Strenuous',
+      title: "Physical",
+      logo: "/src/assets/icons/meter.svg",
+      besideLogo: "Strenuous",
     },
     {
-      title : 'Group Size',
-      logo: '/src/assets/icons/group.svg',
-      besideLogo: '8',
+      title: "Group Size",
+      logo: "/src/assets/icons/group.svg",
+      besideLogo: "8",
     },
     {
-      title : 'Age',
-      logo: '/src/assets/icons/age.svg',
-      besideLogo: '7+',
+      title: "Age",
+      logo: "/src/assets/icons/age.svg",
+      besideLogo: "7+",
     },
     {
-      title : 'Season',
-      logo: '/src/assets/icons/season.svg',
-      besideLogo: 'Apr-Sep',
+      title: "Season",
+      logo: "/src/assets/icons/season.svg",
+      besideLogo: "Apr-Sep",
     },
-  ]
+  ];
   return (
     <div>
       <Tabs value={activeTab}>
@@ -115,23 +119,28 @@ function TabsOverview() {
           ))}
         </TabsHeader>
         <TabsBody>
-          {data.map(({ value, desc }) => (
-            <TabPanel key={value} value={value}>
+          
+            <TabPanel key="overview" value="overview">
               <div className="flex gap-16">
-                <Image src={Description} className={`self-start`}/>
+                <Image src={Description} className={`self-start`} />
                 <div>
-                  <Paragraph className="text-[#1D293F] mb-5" text={desc} />
+                  <Paragraph className="text-[#1D293F] mb-5" text={data.at(0).desc} />
                   <Link className="text-[#99A3AD] font-poppins text-base">
                     Show More...
                   </Link>
                   <div className="flex flex-wrap gap-y-7 mt-14">
-                    {info.map(({title,logo,besideLogo}) => (
+                    {info.map(({ title, logo, besideLogo }) => (
                       <div key={title} className="basis-1/3">
-                        <Subheading text={title} className='text-base text-[#546179]'/>
+                        <Subheading
+                          text={title}
+                          className="text-base text-[#546179]"
+                        />
                         <div className="flex items-center gap-2 mt-7">
-                          <Image src={logo}/>
-                          <Subheading text={besideLogo} className='text-[#1D293F] font-medium'/>
-                          
+                          <Image src={logo} />
+                          <Subheading
+                            text={besideLogo}
+                            className="text-[#1D293F] font-medium"
+                          />
                         </div>
                       </div>
                     ))}
@@ -139,7 +148,38 @@ function TabsOverview() {
                 </div>
               </div>
             </TabPanel>
-          ))}
+            <TabPanel key='iternary' value="iternary">
+            <div className="flex gap-16">
+                <Image src={iternary} className={`self-start`} />
+                <div>
+                  
+                </div>
+            </div>
+            </TabPanel>
+            <TabPanel key='inclusions' value="inclusions">
+            <Image src={inclusions} className={`self-start`} />
+                <div>
+                  
+                </div>
+            </TabPanel>
+            <TabPanel key='reviews' value="reviews">
+            <Image src={review} className={`self-start`} />
+                <div>
+                  
+                </div>
+            </TabPanel>
+            <TabPanel key='faq' value="faq">
+            <Image src={faq} className={`self-start`} />
+                <div>
+                  
+                </div>
+            </TabPanel>
+            <TabPanel key='essential' value="essential">
+            <Image src={iternary} className={`self-start`} />
+                <div>
+                  
+                </div>
+            </TabPanel>
         </TabsBody>
       </Tabs>
     </div>
